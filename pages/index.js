7e053ed3a15ee8966648d0c7f3aefae1f52ef95f
Paddybaba <53314770+Paddybaba@users.noreply.github.com>
2021-02-21 22:53:34 +0530

    Starting the project

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,50 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import tachyons from 'tachyons'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Surgery Log</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <main className="">
+        <h1 className="f3 tc">
+          Welcome to Surgery Log 
         </h1>
+      
 
-        <p className={styles.description}>
+        <form className="mw5 mw7-ns center bg-transparent pa3 ba b--silver br3">
+          <div>
+            <label htmlFor="email_address">Email </label><br></br>
+            <input 
+                id="email_address" 
+                type="email" 
+                name="email_address" 
+                className="mt1 ba b--silver br2"
+                pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/" 
+                required></input>
+          </div>
+          <div>
+            <label htmlFor="password">Password </label><br></br>
+            <input id="password" type="password" name="password" className="mt1 ba b--silver br2"></input>
+          </div>
+          <div>
+            <input type="button" id="submit_login" value="Submit" className="mt1 ba b--silver br2"></input>
+          </div>
+          <div>
+            <Link href="/webpages/register">
+             <a>New User, register here !!</a>
+            </Link>
+              
+            
+          </div>
+        </form>
+
+        {/* <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
@@ -47,7 +77,7 @@ export default function Home() {
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
-        </div>
+        </div> */}
       </main>
 
       <footer className={styles.footer}>
@@ -63,3 +93,6 @@ export default function Home() {
     </div>
   )
 }
+
+
+
